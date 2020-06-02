7457a8247ab91fd67079b6cab06ecb09d1e22cb5
Paulo Bernardo <paulo.bernardo@ilhasoft.com.br>
2020-06-02 10:47:45 -0300

    added rocket endpoint for sending livechat messages and closing livechat chats

--- endpoints/RocketEndpoint.ts
+++ endpoints/RocketEndpoint.ts
@@ -1,5 +1,6 @@
 import { ApiEndpoint, IApiResponse, IApiRequest, IApiEndpointInfo } from "@rocket.chat/apps-engine/definition/api";
 import { IRead, IModify, IHttp, IPersistence } from "@rocket.chat/apps-engine/definition/accessors";
+import { RapidproUtils } from "../utils/RapidproUtils";
 
 export class RocketEndpoint extends ApiEndpoint {
     public path = "rocket/webhook";
@@ -13,9 +14,45 @@ export class RocketEndpoint extends ApiEndpoint {
         persis: IPersistence
     ) : Promise<IApiResponse> {
 
-        this.app.getLogger().debug(request)
-        console.log(">>: ", request)
+        const baseUrl = await read.getEnvironmentReader().getSettings().getValueById("base_url")
+        const authToken = await read.getEnvironmentReader().getSettings().getValueById("push_token")
+        const closeTicket = await read.getEnvironmentReader().getSettings().getValueById("close_tckt_flow")
+
+        const rapidProUtils = new RapidproUtils(read, http, baseUrl, authToken, closeTicket);
+
+        const rc = request.content
+        console.log("Request Content Object: ", rc)
+
+        const data = {
+            visitor: rc.visitor,
+            agent: rc.agent,
+            contactUuid: rc.visitor.customFields.contactUuid,
+            token: rc.visitor.token,
+            type: rc.type,
+            tags: rc.tags,
+            messages: rc.messages,
+        }
+
+        let contacts;
+        if(data.contactUuid) {
+            contacts = [data.contactUuid]
+        } else {
+            contacts = [data.token]
+        }
+
+        // TODO: Check if serialization is necessary
+
+        if(data.type === "Message") {
+            await rapidProUtils.broadcastMessages(data.messages, contacts);
+        } else if(data.type === "LivechatSession") {
+            const extra = {
+                agent: data.agent,
+                livechat: data
+            }
+           await rapidProUtils.closeSession(extra, contacts);
+        }        
 
         return this.success();
     }
+
 }
