64916c1bf2f7965c2e6ee6540885acad822a664f
Paulo Bernardo <paulo.bernardo@ilhasoft.com.br>
2020-06-15 12:26:20 -0300

    fixed errors after merge

--- utils/DateUtils.ts
+++ utils/DateUtils.ts
@@ -2,7 +2,7 @@
 function getNowDate(): string {
     const date = new Date();
     const now = date.toJSON().slice(0, 10);
-    return now.concat(' 00:00:00+00:00');
+    return now.concat('T00:00:00.000');
 }
 
 export {
